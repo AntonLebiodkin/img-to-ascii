dbb2cacab291b2cfeb3bef94d700ee7dda764626
Антон Лебьодкін <anton.lebiodkin@zeocamp-32.zeo.lcl>
2016-07-14 18:22:12 +0300

    added project files

---
+++ image.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anton.lebiodkin
+ * Date: 7/14/16
+ * Time: 11:53 AM
+ */
+
+$image = $_POST["image"];
+print $image;
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+#include "image.php";
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ASCII Converter</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2 class="header text-center">ASCII Converter (Image to ASCII)</h2>
+    <form class="form-inline form-block col-md-4 col-md-offset-4" method="post">
+        <div class="form-group">
+            <label for="file-load">Load image:</label>
+            <input type="file" class="form-control" id="file-load" value="image">
+        </div>
+        <button type="submit" class="btn btn-primary">Load</button>
+    </form>
+</body>
+</html>
